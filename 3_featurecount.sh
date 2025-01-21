ebd2033f19eb93678a135ee7d838000a6dc0d861
Valentin Muller <vmuller@login8.hpc.binf.unibe.ch>
2025-01-21 15:01:08 +0100

    Adding comments for script 3_featurecount

--- 3_featurecount.sh
+++ 3_featurecount.sh
@@ -19,12 +19,14 @@ ANNOTATIONFILE="$WORKDIR/reference_genome/Mus_musculus.GRCm39.113.gtf"
 #Create the directory for the error and output file if not present
 mkdir -p $LOGDIR
 
+#Create the output directory
 mkdir -p $OUTDIR
 
-#take the sample name, path to the read1 and read2 line by line 
+#Take the sample name, path to the read1 and read2 line by line 
 SAMPLE=`awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $1; exit}' $SAMPLELIST`
 READ1=`awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $2; exit}' $SAMPLELIST`
 READ2=`awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $3; exit}' $SAMPLELIST`
 
+#Generate the table of counts
 apptainer exec --bind $WORKDIR /containers/apptainer/subread_2.0.1--hed695b0_0.sif featureCounts -T4 -p -s2 -Q10 -t exon -g gene_id -a $ANNOTATIONFILE -o "$OUTDIR/gene_count.txt" $MAPPINGDIR/*sorted.bam
 
